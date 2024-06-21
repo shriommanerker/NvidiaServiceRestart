eb54d9cbc59aa203ff45ba860d4ed7325f4122a6
Shriom Manerker <shriommanerker@ymail.com>
2024-06-21 22:30:41 +0530

    Create RestartNvidiaServiecs.bat

---
+++ RestartNvidiaServiecs.bat
@@ -0,0 +1,20 @@
+sc stop "NVDisplay.ContainerLocalSystem"
+timeout /t 5 /nobreak
+sc stop "NvContainerLocalSystem"
+timeout /t 5 /nobreak
+cls
+
+sc query "NvContainerLocalSystem"
+sc query "NVDisplay.ContainerLocalSystem"
+
+timeout /t 3 /nobreak
+sc start "NvContainerLocalSystem"
+timeout /t 5 /nobreak
+sc start "NVDisplay.ContainerLocalSystem"
+timeout /t 5 /nobreak
+
+sc query "NvContainerLocalSystem"
+sc query "NVDisplay.ContainerLocalSystem"
+
+timeout /t -1
+timeout /t -1
